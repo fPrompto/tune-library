ac26fd52de58ce95efc79d18eeff5865674da8b5
Fernando F Monteiro <fernando.monteiro.f11@gmail.com>
2024-09-19 16:22:06 -0300

    :sparkles: feat: Função handleClick adicionada a tela inicial para ir para a pagina de detalhes após um item ser selecionado

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { getAlbumData } from '@/api/database';
 
 import '../styles/Home.css';
@@ -8,6 +9,21 @@ import '../styles/Home.css';
 const Home: React.FC = () => {
   const [albumData, setAlbumData] = useState([{}]);
   const [isLoading, setIsLoading] = useState(true);
+
+  const router = useRouter();
+
+
+  const handleClick = (e: React.MouseEvent<HTMLUListElement>) => {
+    // Check if the clicked target is an <li> element
+    const target = e.target as HTMLElement;
+    const albumItem = target.closest('li'); // Get closest <li> ancestor
+
+    if (albumItem) {
+      const albumId = albumItem.id; // Directly access id from the <li>
+      console.log('album id =>', albumId);
+      router.push(`/album/${albumId}`);
+    }
+  };
 
   useEffect(() => {
     setIsLoading(true);
@@ -26,7 +42,7 @@ const Home: React.FC = () => {
     <div>Loading... me deleta depois</div>
   ) : (
     <div className='main-div'>
-      <ul role='list' className='divide-y divide-gray-100'>
+      <ul role='list' className='divide-y divide-gray-100' onClick={handleClick}>
           {albumData.map((a) => {
             const isoDateString = a.launch_date;
             const dateObject = new Date(isoDateString);
@@ -37,7 +53,7 @@ const Home: React.FC = () => {
             });
 
             return (
-              <li key={a.name} className='flex justify-between gap-x-6 py-5'>
+              <li key={a.name} id={a.id} className='flex justify-between gap-x-6 py-5'>
                 <div className='flex min-w-0 gap-x-4'>
                   {/* <img
                 alt=''
