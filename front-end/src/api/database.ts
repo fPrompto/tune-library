2e50c13582c5608251bf7e1761da9a631b542fbf
Fernando F Monteiro <fernando.monteiro.f11@gmail.com>
2024-09-19 18:51:18 -0300

    :sparkles: feat: Página para listar todas as músicas do banco de dados

--- front-end/src/api/database.ts
+++ front-end/src/api/database.ts
@@ -36,3 +36,21 @@ export const getAlbumDataById = (id: string) => {
     });
   });
 };
+
+export const getAllMusics = () => {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: `${BACKEND_URL}/music/find/all`,
+      type: 'GET',
+      dataType: 'json',
+      success: function (response) {
+        console.log('data: ', response);
+        resolve(response);
+      },
+      error: function (_jqXHR, textStatus, errorThrown) {
+        console.error('error: ', textStatus, errorThrown);
+        reject(new Error('failed to fetch album data'));
+      },
+    });
+  });
+};
---
+++ front-end/src/app/(user)/musics/page.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { getAllMusics } from '@/api/database';
+import Loading from '@/app/components/Loading';
+import List from '@/app/components/List';
+
+const Home: React.FC = () => {
+  const [albumData, setAlbumData] = useState([{}]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const router = useRouter();
+
+  const handleClick = (e: React.MouseEvent<HTMLUListElement>) => {
+    // Check if the clicked target is an <li> element
+    const target = e.target as HTMLElement;
+    const albumItem = target.closest('li'); // Get closest <li> ancestor
+
+    if (albumItem) {
+      const albumId = albumItem.id; // Directly access id from the <li>
+      console.log('album id =>', albumId);
+      router.push(`/album/${albumId}`);
+    }
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    getAllMusics()
+      .then((data) => {
+        console.log('Received album data:', data);
+        setAlbumData(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  }, []);
+
+  return isLoading ? (
+    <Loading />
+  ) : (
+    <>
+      <h1 className='page-title'>Músicas</h1>
+      <List data={albumData} clickFunction={handleClick} />
+    </>
+  );
+};
+
+export default Home;
